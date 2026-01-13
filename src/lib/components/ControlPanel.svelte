38bb3578f17499e3206af89571621bdcadee5821
Marek Jóźwiak <168720167+66HEX@users.noreply.github.com>
2026-01-14 00:49:21 +0100

    style(control-panel): change trigger icon

--- src/lib/components/ControlPanel.svelte
+++ src/lib/components/ControlPanel.svelte
@@ -36,20 +36,16 @@
 		aria-label="Toggle Artifact Controls"
 	>
 		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			viewBox="0 0 24 24"
-			fill="currentColor"
 			class="h-6 w-6 transition-transform duration-500 group-hover:rotate-180"
+			xmlns="http://www.w3.org/2000/svg"
+			width="32"
+			height="32"
+			fill="currentColor"
+			viewBox="0 0 256 256"
+			><path
+				d="M216,32H152a8,8,0,0,0-6.34,3.12l-64,83.21L72,108.69a16,16,0,0,0-22.64,0l-12.69,12.7a16,16,0,0,0,0,22.63l20,20-28,28a16,16,0,0,0,0,22.63l12.69,12.68a16,16,0,0,0,22.62,0l28-28,20,20a16,16,0,0,0,22.64,0l12.69-12.7a16,16,0,0,0,0-22.63l-9.64-9.64,83.21-64A8,8,0,0,0,224,104V40A8,8,0,0,0,216,32ZM52.69,216,40,203.32l28-28L80.68,188Zm70.61-8L48,132.71,60.7,120,136,195.31ZM208,100.06l-81.74,62.88L115.32,152l50.34-50.34a8,8,0,0,0-11.32-11.31L104,140.68,93.07,129.74,155.94,48H208Z"
+			></path></svg
 		>
-			<path
-				d="M12 2l2.4 7.2h7.6l-6 4.8 2.4 7.2-6-4.8-6 4.8 2.4-7.2-6-4.8h7.6z"
-				fill="none"
-				stroke="currentColor"
-				stroke-width="1.5"
-				stroke-linejoin="round"
-			/>
-			<circle cx="12" cy="12" r="3" class="opacity-50" />
-		</svg>
 	</button>
 
 	{#if isOpen}
